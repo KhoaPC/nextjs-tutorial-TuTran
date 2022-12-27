2aec252a5adbbf10c4e186cc919aa45d3494f0d8
KhoaPC <luongvankhoapc@gmail.com>
2022-12-27 08:51:32 +0700

    22Dec27 KB get data by function `getStaticProps` #2

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,13 +1,31 @@
 import Head from "next/head";
+import Link from "next/link";
+import { use } from "react";
 
-function User() {
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { allUser: data },
+  };
+};
+
+function User({ allUser }) {
   return (
     <>
       <Head>
         <title>NextJS | User</title>
         <meta name="keywords" content="NextJS" />
       </Head>
-      <h1>User Page</h1>
+      <div>
+        <h1>User:</h1>
+        {allUser.map((user) => (
+          <Link href={`/user/${user.id}`} key={user.id}>
+            <h4>{user.name}</h4>
+          </Link>
+        ))}
+      </div>
     </>
   );
 }
